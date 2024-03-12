d522050b70138bf1dff90a21caacc327214e02ad
capoma <pomacristian421@gmail.com>
2024-03-12 17:42:57 -0500

    Feat: Agregar campos para la respuesta de la solicitudes de tc

--- src/Application/TarjetasCredito/ObtenerSolicitudes/ResGetSolicitudes.cs
+++ src/Application/TarjetasCredito/ObtenerSolicitudes/ResGetSolicitudes.cs
@@ -11,9 +11,12 @@ namespace Application.TarjetasCredito.ObtenerSolicitudes
         {
             public int int_id { get; set; }
             public int int_ente { get; set; }
+            public string str_identificacion { get; set; } = string.Empty;
+            public string str_nombres { get; set; } = string.Empty;
             [JsonIgnore]
             public int int_tipo_tarjeta { get; set; }
             public string str_tipo_tarjeta { get; set; } = string.Empty;
+            public string str_calificacion { get; set; } = string.Empty;
             public decimal dec_cupo_solicitado { get; set; }
             public DateTime dtt_fecha_solicitud { get; set; }
             public string str_usuario_crea { get; set; } = string.Empty;
