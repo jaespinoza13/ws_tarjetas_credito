831ffd721d094ac8fd73cfde04d91b464a87ffd9
jespinoza <jespinoza@mego.net>
2024-05-09 17:57:11 -0500

    Fix: Se agrega variable para recuperar el ente de socio

--- src/Domain/Entities/DatosCliente/DatosCliente.cs
+++ src/Domain/Entities/DatosCliente/DatosCliente.cs
@@ -2,20 +2,21 @@
 {
     public class DatosCliente
     {
-        public string? str_nombres { get; set; } = string.Empty;
-        public string? str_apellido_paterno { get; set; } = string.Empty;
-        public string? str_apellido_materno { get; set; } = string.Empty;
-        public string? str_fecha_nacimiento { get; set; } = string.Empty;
-        public string? str_nivel_educacion { get; set; } = string.Empty;
-        public string? str_codigo_profesion { get; set; } = string.Empty;
-        public string? str_actividad_economica { get; set; } = string.Empty;
-        public string? str_ocupacion { get; set; } = string.Empty;
-        public string? str_estado_civil { get; set; } = string.Empty;
-        public string? str_nacionalidad { get; set; } = string.Empty;
-        public string? str_sexo { get; set; } = string.Empty;
-        public string? str_tipo_persona { get; set; } = string.Empty;
-        public string? str_medio_informacion { get; set; } = string.Empty;
-        public string? str_calificacion_riesgo { get; set; } = string.Empty;
+        public int int_ente {  get; set; } 
+        public string str_nombres { get; set; } = string.Empty;
+        public string str_apellido_paterno { get; set; } = string.Empty;
+        public string str_apellido_materno { get; set; } = string.Empty;
+        public string str_fecha_nacimiento { get; set; } = string.Empty;
+        public string str_nivel_educacion { get; set; } = string.Empty;
+        public string str_codigo_profesion { get; set; } = string.Empty;
+        public string str_actividad_economica { get; set; } = string.Empty;
+        public string str_ocupacion { get; set; } = string.Empty;
+        public string str_estado_civil { get; set; } = string.Empty;
+        public string str_nacionalidad { get; set; } = string.Empty;
+        public string str_sexo { get; set; } = string.Empty;
+        public string str_tipo_persona { get; set; } = string.Empty;
+        public string str_medio_informacion { get; set; } = string.Empty;
+        public string str_calificacion_riesgo { get; set; } = string.Empty;
     }
 
 }
