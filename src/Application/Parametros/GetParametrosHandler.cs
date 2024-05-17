ad038450cf19d3df119b3637a848c43202e70f07
jespinoza <jespinoza@mego.net>
2024-05-17 12:39:55 -0500

    Fix: Se agrega la funcionalidad para obtener los parametros en base al sistema

---
+++ src/Application/Parametros/GetParametrosHandler.cs
@@ -0,0 +1,58 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using Application.TarjetasCredito.ComentariosGestion;
+using Application.TarjetasCredito.InterfazDat;
+using Domain.Entities.ComentariosGestion;
+using Domain.Parameters;
+using Infrastructure.Services;
+using MediatR;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Parametros;
+
+public class GetParametrosHandler : IRequestHandler<ReqGetParametros, ResGetParametros>
+{
+    private readonly ApiSettings _settings;
+    private readonly ILogs _logsService;
+    private readonly string str_clase;
+    private readonly string str_operacion;
+    public readonly IMemoryCache _memoryCache;
+    public GetParametrosHandler(IOptionsMonitor<ApiSettings> options, ILogs logsService, IMemoryCache memoryCache)
+    {
+        _settings = options.CurrentValue;
+        _logsService = logsService;
+        this.str_clase = GetType().FullName!;
+        str_operacion = "GET_PARAMETROS_MEMORIA";
+        _memoryCache = memoryCache;
+    }
+    public async Task<ResGetParametros> Handle(ReqGetParametros request, CancellationToken cancellationToken)
+    {
+        ResGetParametros respuesta = new();
+        respuesta.LlenarResHeader( request );
+        try
+        {
+            var lst_parametros = _memoryCache.Get<List<Parametro>>( "Parametros_back" )!;
+            respuesta.lst_parametros = lst_parametros;
+            var str_codigo = '0';
+            var str_error = "";
+            respuesta.str_res_codigo = str_codigo.ToString().Trim().PadLeft( 3, '0' );
+           
+        }
+        catch (Exception e)
+        {
+
+            Console.WriteLine( e.ToString() );
+            await _logsService.SaveExceptionLogs( respuesta, str_operacion, MethodBase.GetCurrentMethod()!.Name, str_clase, e );
+            throw new ArgumentException( respuesta.str_id_transaccion );
+        }
+
+        return respuesta;
+    }
+}
---
+++ src/Application/Parametros/ReqGetParametros.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Parametros;
+
+public class ReqGetParametros : ResGetParametros, IRequest<ResGetParametros>
+{
+    public int int_id_sis {  get; set; }
+}
